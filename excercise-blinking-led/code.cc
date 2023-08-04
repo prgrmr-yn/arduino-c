417c5f23c4eafcc248cd57a0ecb3eacc8cd43789
Yatin Nagpal <nnega4428@gmail.com>
2023-08-04 19:19:55 +1000

    added code

---
+++ excercise-blinking-led/code.cc
@@ -0,0 +1,18 @@
+// Define the pins for the LEDs
+const int yellowLedPin = 6;
+const int redLedPin = 9;
+
+// Number of times to blink each LED
+const int numBlinks = 3;
+
+// Time (in milliseconds) for each LED blink
+const int blinkInterval = 500;
+
+void setup() {
+  // Set the LED pins as OUTPUT
+  pinMode(yellowLedPin, OUTPUT);
+  pinMode(redLedPin, OUTPUT);
+}
+
+void loop() {
+  }
